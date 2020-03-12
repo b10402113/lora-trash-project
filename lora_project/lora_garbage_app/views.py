e833c077011a4951a05ded3e4204c6417910c6ce
Andy Cheng <b10402113@gmail.com>
2020-03-12 15:20:24 +0800

    修改readme

--- lora_project/lora_garbage_app/views.py
+++ lora_project/lora_garbage_app/views.py
@@ -33,52 +33,50 @@ def event(request):
 
 def trash_detail(request,trash_id):
     trash_can = models.TrashInfo.objects.get(id=trash_id)
-    hnumber = models.TrashHistory.objects.filter(trash_can_name__tloaction_name=trash_can.tloaction_name).count()
-    if hnumber > 10:
-        trash_infomation = models.TrashHistory.objects.filter(trash_can_name__tloaction_name=trash_can.tloaction_name)[hnumber-10:hnumber]
-    else:
-        trash_infomation = models.TrashHistory.objects.filter(trash_can_name__tloaction_name=trash_can.tloaction_name)
-
-    tweight = []
-    theight = []
-    times = []
-    hour_data = []
-    hour_dict={}
-    hour_dict2={}
-    hour_dict_height={}
-    for trash in trash_infomation:
-        tweight.append(trash.trash_history_weight)
-        theight.append(trash.trash_history_height)
-        times.append(trash.trash_history_time.strftime("%H時%M分%S秒"))
-
-        hour_data.append((trash.trash_history_time.hour,trash.trash_history_weight,trash.trash_history_height))
-    for i in range(1,25):
-        hour_dict[str(i)] = [0,0,0]
-    for (hour,weight,height) in hour_data:
-        hour_dict[str(hour)][0] += 1
-        hour_dict[str(hour)][1] += weight
-        hour_dict[str(hour)][2] += height
-        #     算出平均值
-    for i in range(1, 25):
-        if hour_dict[str(i)][0] != 0 and hour_dict[str(i)][1] != 0:
-            num = hour_dict[str(i)][0]
-            hour_dict[str(i)][0] = hour_dict[str(i)][1]/num
-            if hour_dict[str(i)][2] != 0:
-                hour_dict[str(i)][2] = hour_dict[str(i)][2] / num
-
-        hour_dict2[str(i)] = hour_dict[str(i)][0]
-        hour_dict_height[str(i)] = hour_dict[str(i)][2]
-
-    print(hour_dict_height)
-
-    data = {
-        "time":times,
-        "weight":tweight,
-        "height":theight,
-        "hour_dict2":hour_dict2,
-        "hour_dict_height":hour_dict_height
-    }
-
+    # hnumber = models.TrashHistory.objects.filter(trash_can_name__tloaction_name=trash_can.tloaction_name).count()
+    # if hnumber > 10:
+    #     trash_infomation = models.TrashHistory.objects.filter(trash_can_name__tloaction_name=trash_can.tloaction_name)[hnumber-10:hnumber]
+    # else:
+    #     trash_infomation = models.TrashHistory.objects.filter(trash_can_name__tloaction_name=trash_can.tloaction_name)
+    #
+    # tweight = []
+    # theight = []
+    # times = []
+    # hour_data = []
+    # hour_dict={}
+    # hour_dict2={}
+    # hour_dict_height={}
+    # for trash in trash_infomation:
+    #     tweight.append(trash.trash_history_weight)
+    #     theight.append(trash.trash_history_height)
+    #     times.append(trash.trash_history_time.strftime("%H時%M分%S秒"))
+    #
+    #     hour_data.append((trash.trash_history_time.hour,trash.trash_history_weight,trash.trash_history_height))
+    # for i in range(1,25):
+    #     hour_dict[str(i)] = [0,0,0]
+    # for (hour,weight,height) in hour_data:
+    #     hour_dict[str(hour)][0] += 1
+    #     hour_dict[str(hour)][1] += weight
+    #     hour_dict[str(hour)][2] += height
+    #     #     算出平均值
+    # for i in range(1, 25):
+    #     if hour_dict[str(i)][0] != 0 and hour_dict[str(i)][1] != 0:
+    #         num = hour_dict[str(i)][0]
+    #         hour_dict[str(i)][0] = hour_dict[str(i)][1]/num
+    #         if hour_dict[str(i)][2] != 0:
+    #             hour_dict[str(i)][2] = hour_dict[str(i)][2] / num
+    #
+    #     hour_dict2[str(i)] = hour_dict[str(i)][0]
+    #     hour_dict_height[str(i)] = hour_dict[str(i)][2]
+    #
+    #
+    # data = {
+    #     "time":times,
+    #     "weight":tweight,
+    #     "height":theight,
+    #     "hour_dict2":hour_dict2,
+    #     "hour_dict_height":hour_dict_height
+    # }
 
     return render(request, 'trash_detail.html',locals())
 
@@ -121,7 +119,6 @@ def trash_ajax_data(request,trash_id):
         hour_dict2[str(i)] = hour_dict[str(i)][0]
         hour_dict_height[str(i)] = hour_dict[str(i)][2]
 
-    print(hour_dict_height)
 
     data = {
         "time":times,
@@ -151,9 +148,7 @@ def post_trash_data(request):
             trash_id = request.POST.get('trash_id')
             weight = request.POST.get('weight')
             height = request.POST.get('height')
-        # trash_id = json_data["trash_id"]
-        # weight = json_data["weight"]  # 获取name
-        # height = json_data["height"]  # 获取gender
+
         trash_data = models.TrashHistory()
         trash_data.trash_history_height = height
         trash_data.trash_history_weight = weight
@@ -165,21 +160,27 @@ def post_trash_data(request):
         # 判斷是否寄信
         gmail_user = 'andycheeehigh@gmail.com'
         gmail_password = 'a7935776'  # your gmail password
-        if int(weight) > 2000 and trash_can.send_email_bit is False:
-            trash_can.send_email_bit = True
-            msg = (MIMEText('<h1>垃圾過量警告！</h1><p>地點：'+str(trash_can.tloaction_name)+'</p>重量已經超過20kg，請盡快去收垃圾！</p>','html','utf-8'))
-            msg['Subject'] = 'Lora垃圾管理系統通知（請勿回覆）'
-            msg['From'] = gmail_user
-            msg['To'] = 'b10402113@gmail.com'
-            print('===信件連結開始===')
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.ehlo()
-            server.login(gmail_user, gmail_password)
-            server.send_message(msg)
-            server.quit()
-            print('===信件已經寄出===')
-        elif int(weight) < 100 and trash_can.send_email_bit:
-            trash_can.send_email_bit = False
+        try:
+            if (int(weight) > 2000 or int(height) > 20) and trash_can.send_email_bit is False:
+                trash_can.send_email_bit = True
+                if int(weight) > 2000:
+                    msg = (MIMEText('<h1>垃圾過量警告！</h1><p>地點：'+str(trash_can.tloaction_name)+'</p>重量已經超過20kg，請盡快去收垃圾！</p>','html','utf-8'))
+                else:
+                    msg = (MIMEText('<h1>垃圾過量警告！</h1><p>地點：'+str(trash_can.tloaction_name)+'</p>高度已經過高，請盡快去收垃圾！</p>','html','utf-8'))
+                msg['Subject'] = 'Lora垃圾管理系統通知（請勿回覆）'
+                msg['From'] = gmail_user
+                msg['To'] = 'b10402113@gmail.com'
+                print('===信件連結開始===')
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.ehlo()
+                server.login(gmail_user, gmail_password)
+                server.send_message(msg)
+                server.quit()
+                print('===信件已經寄出===')
+            elif int(weight) < 100 and trash_can.send_email_bit:
+                trash_can.send_email_bit = False
+        except:
+            print("網路錯誤")
 
         # 存取資料庫
         trash_can.save()
@@ -188,6 +189,7 @@ def post_trash_data(request):
 
         return redirect(event)
     else:
+        print("非POST請求")
         return redirect(event)
 
 def path_generate(request):
